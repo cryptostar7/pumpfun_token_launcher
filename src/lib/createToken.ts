5bc6cd01ae06bcbe1b9ff461212782f5cfcbf0f0
cryptostar7 <133494886+cryptostar7@users.noreply.github.com>
2025-03-25 03:51:57 +0800

    Update createToken.ts

--- src/lib/createToken.ts
+++ src/lib/createToken.ts
@@ -24,7 +24,7 @@ export async function createToken(form: any) {
     formData.append("initialBuyAmount", form.initialBuyAmount);
 
     // // Create IPFS metadata storage
-    const response = await axios.post('https://katsumoto-pumpfun-token-launcher.vercel.app:3000/createToken', formData);
+    const response = await axios.post('https://pump-token-launcher-backend.vercel.app:3000/createToken', formData);
     console.log("Metadata Response Json", response);
 
 }
