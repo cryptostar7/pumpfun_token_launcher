ea9723a9ee245d1cc602010cff06048696a941e2
lucaslee129 <adfr486233@gmail.com>
2025-04-01 00:03:14 +0800

    chore: while fixing the private key problem

--- src/lib/checkPrivateKeyValid.ts
+++ src/lib/checkPrivateKeyValid.ts
@@ -4,16 +4,19 @@ import bs58 from "bs58";
 export const checkPrivateKeyValid = (privateKey) => {
     try {
 
-        const mintKeypair = Keypair.generate();
-        const mintPrivateKey = mintKeypair.secretKey;
-        const mintCA = mintKeypair.publicKey.toBase58();
+        // const mintKeypair = Keypair.generate();
+        // const mintPrivateKey = mintKeypair.secretKey;
+        // const mintCA = mintKeypair.publicKey.toBase58();
 
-        console.log("Mint Private Key", bs58.encode(mintPrivateKey));
-        console.log("Mint CA", mintCA);
+        // console.log("Mint Private Key", bs58.encode(mintPrivateKey));
+        // console.log("Mint CA", mintCA);
 
         // Create a Keypair from the private key
         const signerKeyPair = Keypair.fromSecretKey(bs58.decode(privateKey)); // Your wallet private key
-        
+        const walletAddress = signerKeyPair.publicKey.toBase58();
+
+        console.log(`Wallet Address: ${walletAddress}`);
+       
         // If no error is thrown, the key is valid
         return true;
     } catch (error) {
